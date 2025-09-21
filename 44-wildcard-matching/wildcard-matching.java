3b717db59fbdbcb9774b2e36f246c5eb41348bb0
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-09-21 22:06:18 +0530

    Time: 2 ms (96.82%) | Memory: 43.7 MB (98.75%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,21 +1,32 @@
 class Solution {
-   public boolean isMatch(String s, String p) {
-    int m = s.length(), n = p.length();
-    boolean[][] dp = new boolean[n+1][m+1];
-    dp[0][0] = true;
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        int sIdx = 0, pIdx = 0;
+        int starIdx = -1, sTmpIdx = -1;
 
-    for (int i = 1; i <= n; i++)
-        if (p.charAt(i-1) == '*')
-            dp[i][0] = dp[i-1][0];
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (p.charAt(i-1) == '*')
-                dp[i][j] = dp[i-1][j] || dp[i][j-1];
-            else if (p.charAt(i-1) == '?' || p.charAt(i-1) == s.charAt(j-1))
-                dp[i][j] = dp[i-1][j-1];
+        while (sIdx < sLen) {          
+            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx))) {
+                ++sIdx;
+                ++pIdx;
+            }          
+            else if (pIdx < pLen && p.charAt(pIdx) == '*') {
+                starIdx = pIdx;
+                sTmpIdx = sIdx;
+                ++pIdx;
+            }        
+            else if (starIdx != -1) {
+                pIdx = starIdx + 1;
+                ++sTmpIdx;
+                sIdx = sTmpIdx;
+            }         
+            else {
+                return false;
+            }
         }
+        while (pIdx < pLen && p.charAt(pIdx) == '*') {
+            ++pIdx;
+        }
+
+        return pIdx == pLen;
     }
-    return dp[n][m];
-}
 }
