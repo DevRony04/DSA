93a69333d90ec86c1b308d92e0d0160032f7dbb4
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-12-15 20:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (67.79%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            int oneDigit = s.charAt(i - 1) - '0';
+            if (oneDigit >= 1) {
+                dp[i] += dp[i - 1];
+            }
+
+            int twoDigits = (s.charAt(i - 2) - '0') * 10 + oneDigit;
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+}
