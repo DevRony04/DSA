d186403fe0da7a8fd228d3c4cf1a9cde1cbc6fd0
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2026-04-06 23:53:17 +0530

    Time: 13 ms (92.75%) | Memory: 50.2 MB (89.86%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -1,45 +1,42 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-         HashSet<String> hset = new HashSet<>();
-        // n, m
-        // O(m)
-        for(int ob[] : obstacles) {
-            hset.add(ob[0]+"#"+ob[1]);
+         // Store obstacles in HashSet (encoded as long)
+        Set<Long> set = new HashSet<>();
+        for (int[] o : obstacles) {
+            long key = (((long) o[0]) << 32) | (o[1] & 0xffffffffL);
+            set.add(key);
         }
 
-        int x = 0, y=0;
-        int dir[][] = {{0,1},{1,0},{0,-1},{-1,0}};
-        int position = 0; // 0=N,1=E,2=S,3=W
+        // Directions: North, East, South, West
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
 
-        // right == -1
-        // position + 1 %4
-        // position + 3
+        int dir = 0; // start facing North
+        int x = 0, y = 0;
+        int maxDist = 0;
 
-        int ans = 0;
-        // O(n*10 )
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                dir = (dir + 1) % 4; // turn right
+            } else if (cmd == -2) {
+                dir = (dir + 3) % 4; // turn left
+            } else {
+                // move step by step
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
 
-        for(int command : commands) {
-            // O(n)
-            // O(n+m)
-            if(command==-1) {
-                position = (position+1)%4;
-            } else if(command==-2) {
-                position = (position+3)%4;
-            }
-            else {
-                for(int i=0; i<command; i++) {
-                    String key = (x+dir[position][0]) +"#"+ (y+dir[position][1]);
-                    if(hset.contains(key))
-                        break;
-                    else {
-                        x = x + dir[position][0];
-                        y = y + dir[position][1];
-                    }
+                    long key = (((long) nx) << 32) | (ny & 0xffffffffL);
+                    if (set.contains(key)) break;
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = Math.max(maxDist, x * x + y * y);
                 }
-                ans = Math.max(ans, x*x+y*y);
             }
         }
 
-        return ans;
+        return maxDist;
     }
 }
